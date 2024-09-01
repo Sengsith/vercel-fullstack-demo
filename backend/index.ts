439938c794cecc6042ce9a95a78f91c7846e820b
Angelo Rosario <angelorosario10@gmail.com>
2024-09-01 01:23:03 -0700

    update backend/index.ts
    implemented cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const port = 3001;
+
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   console.log("Request received.");
